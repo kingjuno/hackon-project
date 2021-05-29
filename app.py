4d9c016c46ccc9561968df0f704a3f40760ca541
Ragify <73241561+Ragify@users.noreply.github.com>
2021-05-29 20:30:00 +0530

    modified front end and added features

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from fetcher import all
+from fetcher import all,specific_data
 app = Flask(__name__)
 
+@app.route('/country/<name>', methods=['GET','POST'])
+def country(name):
+    return render_template('country.html',data=specific_data(name))
+
+@app.route('/country/',methods=['GET','POST'])
+def enter():
+    return render_template('enter_country.html',data=all())
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html',data=all())
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
