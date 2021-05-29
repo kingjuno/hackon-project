54e8b560f86509ad0b5107565a1b44b6e3c0c250
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2021-05-29 20:53:20 +0530

    Added country wise data analytics and fixed backend

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from fetcher import all,specific_data
 app = Flask(__name__)
 
 @app.route('/country/<name>', methods=['GET','POST'])
-def country(name):
-    return render_template('country.html',data=specific_data(name))
+def country(country):
+    return render_template('country.html',data=specific_data(country))
 
 @app.route('/country/',methods=['GET','POST'])
 def enter():
-    return render_template('enter_country.html',data=all())
+    if request.method == 'POST':
+        name = request.form['country']
+        dta=specific_data(name)
+        if dta[0]:
+            return render_template('country.html',data=dta)
+        else:
+            return render_template('index.html',data=all())
+    return render_template('enter_country.html')
 
 @app.route('/')
 def index():
